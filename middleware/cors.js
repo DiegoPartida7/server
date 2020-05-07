d23526ee3eafefd1840de93d1f9c53c13e8ef494
Diego Partida <diego.partida@hotmail.com>
2020-05-06 23:56:47 -0500

    update

--- middleware/cors.js
+++ middleware/cors.js
@@ -3,9 +3,9 @@ function cors(req, res, next) {
     res.header("Access-Control-Allow-Headers", "Content-Type,Authorization");
     res.header("Access-Control-Allow-Methods", "GET,POST,PUT,PATCH,DELETE");
     if (req.method === "OPTIONS") {
-        return res.send(204);
+      return res.send(204);
     }
     next();
-} 
+}
 
 module.exports = cors;
