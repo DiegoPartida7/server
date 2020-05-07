d258b13c43d01f2bf68e6bdd9d1272ae29735438
Diego Partida <diego.partida@hotmail.com>
2020-05-07 00:09:46 -0500

    update

--- public/js/index.js
+++ public/js/index.js
@@ -1,47 +1,95 @@
-function watchStudentsForm(){
-    let studentsForm = document.querySelector('.students-form');
-    let url ='/api/students';
+
+const API_TOKEN = 'password12345';
+
+function addStudentFech( name, id ){
+    let url = '/api/createStudent';
+
+    let data = {
+        name : name,
+        id : Number(id)
+    }
+
+    let settings = {
+        method : 'POST',
+        headers : {
+            Authorization : `Bearer ${API_TOKEN}`,
+            'Content-Type' : 'application/json'
+        },
+        body : JSON.stringify( data )
+    }
+
+    let results = document.querySelector( '.results' );
+
+    fetch( url, settings )
+        .then( response => {
+            if( response.ok ){
+                return response.json();
+            }
+            throw new Error( response.statusText );
+        })
+        .then( responseJSON => {
+            fetchStudents();
+        })
+        .catch( err => {
+            results.innerHTML = `<div> ${err.message} </div>`;
+        });
+}
+
+function fetchStudents(){
+
+    let url = '/api/students';
     let settings = {
         method : 'GET',
         headers : {
-            Authorization : `Bearer ${TOKEN}`
+            Authorization : `Bearer ${API_TOKEN}`
         }
     }
-    let results = document.querySelector('.results');
+    let results = document.querySelector( '.results' );
 
-    fetch(url,settings)
-        .then(response =>{
-            if(response.ok){
+    fetch( url, settings )
+        .then( response => {
+            if( response.ok ){
                 return response.json();
             }
-            throw new Error(response.statusText);
+            throw new Error( response.statusText );
         })
-        .then(responseJson =>{
-            results.innerHTML ="";
-            for (let i = 0; i < responseJson.length; i++) {
-                results.innerHTML += (`<div> ${responseJson[i].name}</div>`);
-                
+        .then( responseJSON => {
+            results.innerHTML = "";
+            for ( let i = 0; i < responseJSON.length; i ++ ){
+                results.innerHTML += `<div> ${responseJSON[i].name} </div>`;
             }
-            console.log(responseJson);
         })
-        .catch( err=>{
-            results.innerHTML = `<div> ${err.message}</div>`;
+        .catch( err => {
+            results.innerHTML = `<div> ${err.message} </div>`;
         });
-    }
+    
+}
 
-studentsForm.addEventListener('submit',(event)=>{
-    event.preventDefault();
-    console.log("working");
-});
+function watchStudentsForm(){
+    let studentsForm = document.querySelector( '.students-form' );
+
+    studentsForm.addEventListener( 'submit', ( event ) => {
+        event.preventDefault();
+
+        fetchStudents();
+    });
+}
 
 function watchAddStudentForm(){
-    let studentsForm = document.querySelector('.add-students-form');
-    studentsForm.addEventListener('submit')
+    let studentsForm = document.querySelector( '.add-student-form' );
 
+    studentsForm.addEventListener( 'submit' , ( event ) => {
+        event.preventDefault();
+        let name = document.getElementById( 'studentName' ).value;
+        let id = document.getElementById( 'studentID' ).value;
+
+        addStudentFech( name, id );
+    })
 }
 
 function init(){
-
+    watchStudentsForm();
+    watchAddStudentForm();
 }
 
 init();
